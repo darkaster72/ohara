80695ac25ee70620ae5a4294200df31f4c45e233
Ankit Sharma <sankit72@gmail.com>
2023-03-12 19:43:59 +0530

    include styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
-import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
 
+import "rc-slider/assets/index.css";
+import "react-rater/lib/react-rater.css";
+import "swiper/swiper.scss";
 import "~/styles/globals.css";
+import "../assets/css/styles.scss";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
