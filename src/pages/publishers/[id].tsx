aa5ca3fb427dfd189afb78f1f5770648e27283c1
Ankit Sharma <sankit72@gmail.com>
2023-03-14 23:23:55 +0530

    update book by author page

--- src/pages/publishers/[id].tsx
+++ src/pages/publishers/[id].tsx
@@ -4,15 +4,17 @@ import { api } from "~/utils/api";
 
 interface Props {}
 
-const AuthorPage: NextPage<Props> = () => {
+const PublisherPage: NextPage<Props> = () => {
   const { id } = useRouter().query;
-  const { isLoading, data } = api.book.getByAuthor.useQuery(id as string, {
+  const { data } = api.book.getByPublisher.useQuery(id as string, {
     enabled: !!id,
   });
   return (
     <div className="container">
-      <h3 className="">Books by Author</h3>
+      <h3 className="">Books by Publisher</h3>
       <div className="flex-con flex gap-2">{data?.map((b) => b.title)}</div>
     </div>
   );
 };
+
+export default PublisherPage;
