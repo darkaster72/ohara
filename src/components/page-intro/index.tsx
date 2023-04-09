2e4c8bdf70dbf79a277d578d856ac808b49a6b43
Ankit Sharma <sankit72@gmail.com>
2023-04-09 20:14:37 +0530

    update imaeg

--- src/components/page-intro/index.tsx
+++ src/components/page-intro/index.tsx
@@ -1,43 +1,37 @@
+import Link from "next/link";
 import SwiperCore, { EffectFade, Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 SwiperCore.use([EffectFade, Navigation]);
 
+const slides = [
+  { imgUrl: "/images/slide-1.jpg", title: "Get the knowledge." },
+  { imgUrl: "/images/slide-2.jpg", title: "For you bookworms." },
+];
+
 const PageIntro = () => {
   return (
     <section className="page-intro">
       <Swiper navigation effect="fade" className="swiper-wrapper">
-        <SwiperSlide>
-          <div
-            className="page-intro__slide"
-            style={{ backgroundImage: "url('/images/slide-1.jpg')" }}
-          >
-            <div className="container">
-              <div className="page-intro__slide__content">
-                <h2>Sale of the summer collection</h2>
-                <a href="#" className="btn-shop">
-                  <i className="icon-right"></i>Shop now
-                </a>
+        {slides.map((slide) => (
+          <SwiperSlide key={slide.title}>
+            <div
+              className="page-intro__slide"
+              style={{ backgroundImage: `url(${slide.imgUrl})` }}
+            >
+              <div className="container">
+                <div className="page-intro__slide__content">
+                  <h2>{slide.title}</h2>
+                  <Link href="/products" className="btn-shop">
+                    <>
+                      <i className="icon-right"></i>Shop now
+                    </>
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div
-            className="page-intro__slide"
-            style={{ backgroundImage: "url('/images/slide-2.jpg')" }}
-          >
-            <div className="container">
-              <div className="page-intro__slide__content">
-                <h2>Make your house into a home</h2>
-                <a href="#" className="btn-shop">
-                  <i className="icon-right"></i>Shop now
-                </a>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
+          </SwiperSlide>
+        ))}
       </Swiper>
 
       <div className="shop-data">
@@ -47,7 +41,7 @@ const PageIntro = () => {
               <i className="icon-shipping"></i>
               <div className="data-item__content">
                 <h4>Free Shipping</h4>
-                <p>On purchases over $199</p>
+                <p>On purchases over 500</p>
               </div>
             </li>
 
